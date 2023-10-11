de7c90e62a534ffedf9fe049148c9b5724c71ee8
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-10-11 18:22:54 +0530

    get-profile-action

---
+++ src/components/Dashboard/MainDashBoard.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import AccountList from "./AccountList";
+import AccountSummary from "./AccountSummary";
+import { getProfileAction } from "../../redux/slice/users/usersSlice";
+
+const MainDashBoard = () => {
+  // ! dispatch
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProfileAction());
+  }, [dispatch]);
+
+  return (
+    <>
+      {loading ? (
+        /* loading */
+        <h2 className="text-center text-green-600 mt-5 text-lg">Loading...</h2>
+      ) : error ? (
+        /* error */
+        <h2 className="text-red-600 text-center mt-5 text-lg">{error}</h2>
+      ) : (
+        <>
+          <AccountSummary profile={profile} />
+          <AccountList profile={profile} />
+        </>
+      )}
+    </>
+  );
+};
+
+export default MainDashBoard;

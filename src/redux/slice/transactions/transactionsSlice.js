58becfb8855f034b558f925184d518d457c94a0e
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-10-24 18:13:15 +0530

    action-to-create-transaction

--- src/redux/slice/transactions/transactionsSlice.js
+++ src/redux/slice/transactions/transactionsSlice.js
@@ -11,3 +11,43 @@ const initialState = {
   isUpdated: false,
   error: null,
 };
+
+// TODO: Create Action Creator - createAsyncThunk
+
+// ! action to create transaction
+export const createTransactionAction = createAsyncThunk(
+  "transaction/create",
+  async (payload, { rejectWithValue, getState, dispatch }) => {
+    // ? destructing payload
+    const { name, transactionType, amount, category, notes } = payload;
+    try {
+      // TODO: get the token
+      const token = getState()?.users?.userAuth?.userInfo?.token;
+
+      // TODO: pass the token to header
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      // TODO: make the request
+      const { data } = await axios.post(
+        `${baseURL}/transactions`,
+        {
+          name,
+          transactionType,
+          amount,
+          category,
+          notes,
+          account: payload.id,
+        },
+        config
+      );
+
+      return data;
+    } catch (error) {
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);

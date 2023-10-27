bf154fd7615efe5b11c189355d724ed08a3bc65a
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-10-27 20:32:29 +0530

    exporting-store

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,3 +2,5 @@ import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "../slice/users/usersSlice";
 import accountsReducer from "../slice/accounts/accountsSlice";
 import transactionsReducer from "../slice/transactions/transactionsSlice";
+
+export default store;

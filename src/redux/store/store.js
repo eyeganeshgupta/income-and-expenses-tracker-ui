93c31230a694093ea8fcdf1af736c7748dc2bc40
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-10-27 20:33:29 +0530

    configure-redux-store

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -3,4 +3,13 @@ import usersReducer from "../slice/users/usersSlice";
 import accountsReducer from "../slice/accounts/accountsSlice";
 import transactionsReducer from "../slice/transactions/transactionsSlice";
 
+// ! store
+const store = configureStore({
+  reducer: {
+    users: usersReducer,
+    accounts: accountsReducer,
+    transactions: transactionsReducer,
+  },
+});
+
 export default store;

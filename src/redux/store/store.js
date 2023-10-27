b0cb551a9d0e4d5c360f7fcfc97b0e406a7b5905
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-10-27 20:32:04 +0530

    required-imports

---
+++ src/redux/store/store.js
@@ -0,0 +1,4 @@
+import { configureStore } from "@reduxjs/toolkit";
+import usersReducer from "../slice/users/usersSlice";
+import accountsReducer from "../slice/accounts/accountsSlice";
+import transactionsReducer from "../slice/transactions/transactionsSlice";

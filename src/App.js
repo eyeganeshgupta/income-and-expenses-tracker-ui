6d9372bb55d56326faec3a3773389923109f0528
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-10-30 23:55:52 +0530

    App-component

--- src/App.js
+++ src/App.js
@@ -10,3 +10,69 @@ import AddAccount from "./components/Forms/AddAccount";
 import EditAccount from "./components/Forms/EditAccount";
 import Login from "./components/Forms/Login";
 import AuthRoute from "./components/AuthRoute/AuthRoute";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/dashboard"
+          element={
+            <AuthRoute>
+              <MainDashBoard />
+            </AuthRoute>
+          }
+        />
+
+        <Route
+          path="/account/:id"
+          element={
+            <AuthRoute>
+              <AccountDetails />
+            </AuthRoute>
+          }
+        />
+
+        <Route
+          path="/add-transaction/:id"
+          element={
+            <AuthRoute>
+              <AddTransaction />
+            </AuthRoute>
+          }
+        />
+
+        <Route
+          path="/edit-transaction/:id"
+          element={
+            <AuthRoute>
+              <EditTransaction />
+            </AuthRoute>
+          }
+        />
+        <Route
+          path="/add-account"
+          element={
+            <AuthRoute>
+              <AddAccount />
+            </AuthRoute>
+          }
+        />
+        <Route
+          path="/edit-account/:id"
+          element={
+            <AuthRoute>
+              <EditAccount />
+            </AuthRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;

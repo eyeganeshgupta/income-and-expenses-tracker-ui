40d756b6aeaf3cd4bd3f1eb3f68c1171fc459e9a
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-10-31 18:51:16 +0530

    provider-store

--- src/index.js
+++ src/index.js
@@ -4,3 +4,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import store from "./redux/store/store";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    
+  </Provider>
+);

0a17257082a65e287c3f75f10238049df8de4cb5
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-10-31 18:51:38 +0530

    render-App-component

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import store from "./redux/store/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </Provider>
 );
